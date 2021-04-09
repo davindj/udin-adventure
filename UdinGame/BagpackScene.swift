8c99f71c1a1368e055d448a58ea8733646c2da89
98prabowo <agungdimas1998@gmail.com>
2021-04-09 13:20:25 +0700

    Adding BagpackScene

---
+++ UdinGame/BagpackScene.swift
@@ -0,0 +1,30 @@
+//
+//  BagpackScene.swift
+//  UdinGame
+//
+//  Created by Dimas A. Prabowo on 09/04/21.
+//
+
+import SpriteKit
+
+class BagpackScene: SKScene {
+    
+    var closeButton: SKNode?
+    
+    override func didMove(to view: SKView) {
+        closeButton = childNode(withName: "closeButton")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let locationButton = touch.location(in: self)
+            let buttonName = atPoint(locationButton)
+            
+            if buttonName.name == "closeButton" {
+                buttonName.run(.setTexture(SKTexture(imageNamed: "bagcloseButton")))
+                
+                
+            }
+        }
+    }
+}
