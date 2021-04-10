6480a1025196a08b8902609bc2d5d50957540951
98prabowo <agungdimas1998@gmail.com>
2021-04-10 14:21:39 +0700

    Adding InteractionYusuf scene

---
+++ UdinGame/InteractionYusuf.swift
@@ -0,0 +1,18 @@
+//
+//  InteractionYusuf.swift
+//  UdinGame
+//
+//  Created by Dimas A. Prabowo on 10/04/21.
+//
+
+import SpriteKit
+
+class InteractionYusuf: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
