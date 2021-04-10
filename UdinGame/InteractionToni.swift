9cb4cb0568d0ef4b06f852c35ae3dd9b95f76b89
98prabowo <agungdimas1998@gmail.com>
2021-04-10 14:21:21 +0700

    Adding InteractionToni scene

---
+++ UdinGame/InteractionToni.swift
@@ -0,0 +1,19 @@
+//
+//  InteractionToni.swift
+//  UdinGame
+//
+//  Created by Dimas A. Prabowo on 10/04/21.
+//
+
+import SpriteKit
+
+class InteractionToni: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
