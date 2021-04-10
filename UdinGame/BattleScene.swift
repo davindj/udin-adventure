a9be45af4de8dabeaf3eb07e9a16eef513036819
Davin Djayadi <davindx88@gmail.com>
2021-04-10 18:58:53 +0700

    Battle Scene, Intro Animation

--- UdinGame/BattleScene.swift
+++ UdinGame/BattleScene.swift
@@ -7,9 +7,53 @@
 
 import SpriteKit
 
-class BattleScene: SKScene{
-    // Component
+struct Dialog {
+    // Parent
+    var dialog: SKNode
+    // Bubble Chat
+    var bubbleChat: SKNode
+    var labelSpeaker: SKLabelNode
+    var labelChat: SKLabelNode
+    // Button
+    var btnGroup: SKNode
     
+    
+    
+    init(dialog: SKNode){
+        self.dialog = dialog
+        // Load Bubble Chat
+        guard let bubbleChat = dialog.childNode(withName: "bubbleChat") else {
+            fatalError("Bubble chat not found!")
+        }
+        guard let labelSpeaker = bubbleChat.childNode(withName: "labelSpeaker") as? SKLabelNode else {
+            fatalError("Label Speaker not found!")
+        }
+        guard let labelChat = bubbleChat.childNode(withName: "labelChat") as? SKLabelNode else {
+            fatalError("Label Chat not found!")
+        }
+        // Load Button Group
+        guard let btnGroup = dialog.childNode(withName: "btnGroup") else {
+            fatalError("Button group chat not found!")
+        }
+        
+        // Assign
+        self.bubbleChat = bubbleChat
+        self.labelSpeaker = labelSpeaker
+        self.labelChat = labelChat
+        self.btnGroup = btnGroup
+        
+        // Fade Out
+        let action = SKAction.fadeOut(withDuration: 0.00001)
+        self.dialog.run(action)
+    }
+}
+
+class BattleScene: SKScene{
+    // CONSTANT
+    static let CHAR_DURATION: TimeInterval = 0.15
+    
+    // Component (Dialog)
+    var dialog: Dialog!
     // Frame
     var framePlayerFront = [SKTexture]()
     
@@ -19,13 +63,17 @@ class BattleScene: SKScene{
     override func didMove(to view: SKView) {
         // Get Component
         self.camera = childNode(withName: "camera") as? SKCameraNode
+        loadDialog()
         // Load Sprite
         loadSprite()
         // Animating
         animateIntroScene{
             self.animateBackground(duration: 1.5)
             self.animateCamera(duration: 1.5)
-            self.animateCharacter()
+            self.animateCharacter{
+                self.animateUdin(duration: 0.5)
+                self.animateDialog()
+            }
         }
     }
     
@@ -35,6 +83,46 @@ class BattleScene: SKScene{
     }
     
     // MARK: Animate Function
+    // Animate Label Text
+    
+    func animateLabel(
+        label: SKLabelNode,
+        text: String,
+        durationPerChar durChar: TimeInterval = CHAR_DURATION,
+        durationPerDot durDot: TimeInterval = CHAR_DURATION,
+        callBack cb: @escaping (()->Void) = {}
+    ){
+        // Hapus text Dulu
+        label.text = ""
+        // SKAction
+        var idx = 0
+        var closureAnim: (()->Void)!
+        closureAnim = {
+            if idx >= text.count{
+                // Sudah Dianimasikan Semua...
+                cb()
+            }else{
+                // Masih Dalam Iterasi
+                let index = text.index(text.startIndex, offsetBy: idx)
+                let currentString = String(text[index])
+                let action = SKAction.sequence([
+                    SKAction.wait(forDuration: currentString == "." ? durDot : durChar),
+                    SKAction.run{
+                        idx += 1
+                        label.text = String(text.prefix(idx))
+                    }
+                ])
+                label.run(action){
+                    closureAnim()
+                }
+            }
+        }
+        // Panggil Function Recursive
+        closureAnim()
+    }
+    
+    
+    // Animate Intro Scene
     func animateIntroScene(callBack cb: @escaping (()->Void) = {}){
         // Change Z Index of bgNode
         guard let bgNode = childNode(withName: "bgIntro") as? SKSpriteNode else {return}
@@ -42,37 +130,14 @@ class BattleScene: SKScene{
         // Animate Text
         let introText: String = "Udin..."
         guard let textIntro = childNode(withName: "textIntro") as? SKLabelNode else{return}
-        textIntro.text = ""
-        // SKAction
-        var idx = 0
-        var closureAnim: (()->Void)!
-        closureAnim = {
-            if idx >= introText.count{
-                // Sudah Dianimasikan Semua...
-                // Delay 1 Detik Terus hapus text
-                let action = SKAction.wait(forDuration: 1)
-                textIntro.run(action){
-                    textIntro.text = ""
-                    cb()
-                }
-            }else{
-                // Masih Dalam Iterasi
-                let index = introText.index(introText.startIndex, offsetBy: idx)
-                let currentString = String(introText[index])
-                let action = SKAction.sequence([
-                    SKAction.wait(forDuration: currentString == "." ? 0.5 : 0.15),
-                    SKAction.run{
-                        idx += 1
-                        textIntro.text = String(introText.prefix(idx))
-                    }
-                ])
-                textIntro.run(action){
-                    closureAnim()
-                }
+        
+        self.animateLabel(label: textIntro, text: introText){
+            let action = SKAction.wait(forDuration: 1)
+            textIntro.run(action){
+                textIntro.text = ""
+                cb()
             }
         }
-        // Panggil animasi
-        closureAnim()
     }
     
     // Animate Background Jadi Transparant
@@ -92,15 +157,58 @@ class BattleScene: SKScene{
         self.camera?.run(action)
     }
     
-    // Animate Player & Udin Character into Scene
+    // Animate Player into Scene
     func animateCharacter(callback cb: @escaping (()->Void) = {}){
+        // Get Player Object
+        guard let player = childNode(withName: "player") as? SKSpriteNode else {
+            fatalError("Player Entity Not Found!")
+        }
         
+        let actionMove = SKAction.move(to: CGPoint(x: -1200, y: -500), duration: 2)
+        let actionAnimating = SKAction.repeatForever(SKAction.animate(with: framePlayerFront, timePerFrame: 0.1))
+
+        player.run(actionAnimating, withKey: "animation")
+        player.run(actionMove){
+            player.removeAllActions()
+            cb()
+        }
+    }
+    
+    // Animate Udin into Scene
+    func animateUdin(duration: TimeInterval, callback cb: @escaping (()->Void) = {}){
+        // Get Udin Object
+        guard let udin = childNode(withName: "udin") as? SKSpriteNode else {
+            fatalError("Udin Entity Not Found!")
+        }
+        // Sprite Udin Geser ke Atas
+        let action = SKAction.moveTo(y: 90, duration: duration)
+        // Call Animation
+        udin.run(action)
+    }
+    
+    // Animate Dialog
+    func animateDialog(callback cb: @escaping (()->Void) = {}){
+        // NOTE: Dialog sudah ada, jadi tidak perlu diLoad
+        // yg dianimasikan dialognya muncul
+        let action = SKAction.fadeIn(withDuration: 0.3)
+        self.dialog.dialog.run(action){
+            cb()
+        }
+    }
+    
+    // MARK: Load Component / Node
+    func loadDialog(){
+        // Coba Dapetin Component Dialog beserta child"nya yg perlu
+        guard let dialog = childNode(withName: "dialog") else {
+            fatalError("Dialog not found!")
+        }
+        self.dialog = Dialog(dialog: dialog)
     }
     
     // MARK: Load Sprite Function
     // Load Semua Textture dari Semua Sprite
     func loadSprite(){
-        framePlayerFront = loadTexture(spriteName: "UdinFront", textureName: "Front")
+        framePlayerFront = loadTexture(spriteName: "PlayerSide", textureName: "Side")
     }
     
     // Load Texture dari suatu sprite yg dipakai
@@ -112,6 +220,10 @@ class BattleScene: SKScene{
         for i in 0..<textureAtlas.textureNames.count{
             let textureCurrent = "\(textureName)\(i)"
             listTexture.append(textureAtlas.textureNamed(textureCurrent))
+        }
+        // Pengecekan apakah Texturenya ketemu
+        if listTexture.isEmpty{
+            fatalError("Texture not found! Sprite=\(name), Texture=\(textureName)")
         }
         return listTexture
     }
