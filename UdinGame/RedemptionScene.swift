b56077766ab77b6921f51b91b808708d5f55b825
Davin Djayadi <davindx88@gmail.com>
2021-04-13 14:06:35 +0700

    Redemption And Navigation

--- UdinGame/RedemptionScene.swift
+++ UdinGame/RedemptionScene.swift
@@ -15,16 +15,57 @@ class RedemptionScene: SKScene{
     // MARK: Constructor
     override func didMove(to view: SKView) {
         if RedemptionScene.isHappyEnding{ // Jika Happy Ending
-            openScene(scene: OutroGoodScene())
+            goodEnd()
         }else if RedemptionScene.N_REDEMPT >= 1{ // Jika Sad Ending
-            openScene(scene: OutroBadScene())
+            badEnd()
         }else{ // Masih Ada kesempatan Redemption
             RedemptionScene.N_REDEMPT += 1
+            self.camera = childNode(withName: "camera") as? SKCameraNode
         }
     }
     
-    func openScene(scene gameScene: SKScene){
-        gameScene.scaleMode = .aspectFill
-        self.view?.presentScene(gameScene, transition: SKTransition.fade(withDuration: 1.0))
+    // MARK: End Function
+    // Function yg dijalankan ketika player sudah mencapai scene terkahir
+    func eofGame(){
+        RedemptionScene.N_REDEMPT = 0
+    }
+    
+    // MARK: Scene Navigation
+    func goodEnd(){
+        eofGame()
+        openScene(scene: OutroGoodScene(fileNamed: "OutroGood0"))
+    }
+    
+    func badEnd(){
+        eofGame()
+        openScene(scene: OutroBadScene(fileNamed: "OutroBad0"))
+    }
+    
+    func battleEnd(){
+        openScene(scene: BattleScene(fileNamed: "BattleScene"))
+    }
+    
+    func openScene(scene gameScene: SKScene?){
+        gameScene!.scaleMode = .aspectFill
+        self.view?.presentScene(gameScene!, transition: SKTransition.fade(withDuration: 1.0))
+    }
+    
+    // MARK: Event Handler
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let locationButton = touch.location(in: self)
+            let node = atPoint(locationButton)
+            let name = node.name
+            switch name{
+            case "labelYes":
+                battleEnd()
+                return
+            case "labelNo":
+                badEnd()
+                return
+            default:
+                print("")
+            }
+        }
     }
 }
